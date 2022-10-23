ddaaf15e03c16fdf862277bf1c2934116a2e7c8f
Therry Hilaire <therry@pop-os.localdomain>
2022-10-23 15:22:31 -0400

    Changed the colorscheme

--- .emacs.d/elpa/proof-general-20220930.1309/proof-general-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "proof-general" "20220930.1309" "A generic Emacs interface for proof assistants"
-  '((emacs "25.2"))
-  :commit "ef18ee4d6ffa160fe563307561f2cad12e9478b5" :maintainer
-  '(nil . "proof-general-maintainers@groupes.renater.fr")
-  :url "https://proofgeneral.github.io/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
